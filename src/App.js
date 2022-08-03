2a457ceb689d1bc76186eb6429223b43b7cb7807
Himanshu rawat <94916323+Himanshu2021rawat@users.noreply.github.com>
2022-08-03 21:16:18 +0530

    First comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+
+
+// import { BrowserRouter as Router,Route,Link,NavLink,Routes} from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState("light"); // wheather dark mode enable or not
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode has Been Enabled", "Succes");
+      document.title = "textUtils-Dark Mode";
+
+      // mesages on the titile bar
+      // setInterval(() => {
+      //   document.title = "textUtils- is amAzing bitch";
+      // }, 2000);
+
+      // setInterval(() => {
+      //   document.title = "textUtils-Now Bitches";
+      // }, 1500);
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has Been Enabled", "Succes");
+      document.title = "textUtils-Light Mode";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+      <Navbar
+        title="TextUtils"
+        aboutText="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+
+      <Alert alert={alert} />
+      <div className="container my-3">
+ <TextForm
+        showAlert={showAlert}
+        heading="Enter The Text To Analyze Below:"
+        mode={mode}/> 
+ </div>
+
+      </>
   );
 }
-
 export default App;
